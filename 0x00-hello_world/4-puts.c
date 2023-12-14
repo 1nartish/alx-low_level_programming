23d895e9849feec32b2e2f8c3127585e6e6dccec
Emmanuel Nartey <nartishrinart@gmail.com>
2023-12-14 09:09:22 -0800

    basic c programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - It entails string characters
+ *
+ * Return: 0 when succesful
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
